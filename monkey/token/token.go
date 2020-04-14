0fcaecceeb4363d2ddb2b689cd80d78201e93e2d
Jumpei Yoshimura <jumpyoshim@gmail.com>
2020-04-15 03:41:46 +0900

    add token type

--- monkey/token/token.go
+++ monkey/token/token.go
@@ -1,5 +1,31 @@
 package token
 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOL     = "EOL"
+
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
 type TokenType string
 
 type Token struct {
